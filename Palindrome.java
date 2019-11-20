2ed7658f35024d453ccc89d8f7aab889fbde6fe0
vaishali-b <38690796+vaishali-b@users.noreply.github.com>
2019-11-20 00:00:32 -0500

    Create Palindrome.java

---
+++ Palindrome.java
@@ -0,0 +1,18 @@
+class Palindrome
+{
+  public static void main(String args[])
+  {
+    String orig, rev = “";
+    Scanner in = new Scanner(System.in);
+
+    System.out.println("Enter a string");
+    orig = in.nextLine();
+    int length = orig.length();
+    for (int i = length - 1; i >= 0; i--)
+      rev = rev + orig.charAt(i);
+    if (orig.equals(rev))
+      System.out.println("The string is a palindrome.");
+    else
+      System.out.println("The string is not a palindrome.");
+  }
+}
