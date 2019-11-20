ca0d3c021d6b84c0198a31c76958c76c228332ac
vaishali-b <38690796+vaishali-b@users.noreply.github.com>
2019-11-20 15:34:51 -0500

    Create FloydTriangle.java

---
+++ FloydTriangle.java
@@ -0,0 +1,20 @@
+class FloydTriangle{
+
+public static void main(String[] args)
+  {
+
+
+ //Floyd Triangle
+      int n=6;
+      int a=1;
+      for(int i=1;i<=n;i++){
+          for(int j=1;j<=i;j++){
+              
+              System.out.print(a+" ");
+              a++;
+              
+          }
+          System.out.println();
+      }
+  }
+}
