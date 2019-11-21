179efc958f32ab3fca9aa4257d87e863dae56b52
vaishali-b <38690796+vaishali-b@users.noreply.github.com>
2019-11-21 14:13:58 -0500

    Create Fractions.java

---
+++ Fractions.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Fractions{
+public static void addFractions(int[] fraction1,int[] fraction2){
+        //find lcm of denominator
+        int sum=1;
+        int lcm=1;
+        int multipleF1=1;
+        int multipleF2=1;
+        for(int i=1;i<fraction1[1];i++){
+            if(((i*fraction1[1])%fraction2[1])==0){
+                lcm=i*fraction1[1];
+                multipleF1=i;
+                multipleF2=lcm/fraction2[1];
+               
+                break;
+            }
+        }
+        //numerator multiplication  
+        int numerator=( fraction1[0]*multipleF1 + fraction2[0]*multipleF2);
+        System.out.println(asFraction(numerator,lcm));
+        
+    }
+    public static int gcm(int a, int b) {
+        return b == 0 ? a : gcm(b, a % b);
+    }
+
+    public static String asFraction(int a, int b) {
+        int gcm = gcm(a, b);
+        return (a / gcm) + "/" + (b / gcm);
+    }
+
+
+
+
+public static void main(String[] args){
+
+      int[] fraction1=  {3,5};    //{3,9};
+      int[] fraction2=  {9,15};    //{2,6};
+      addFractions(fraction1,fraction2);
+      
+    }
+    
+}
+
