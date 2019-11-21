4299d7f0bfe27c08d5322b0e527ba7ebb3a59802
vaishali-b <38690796+vaishali-b@users.noreply.github.com>
2019-11-21 16:09:05 -0500

    Update Fibonacci.java
    Updating series - retrieve Nth element, sum upto given value

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,17 +3,32 @@ class Fibonacci{
 
 public static void main(String[] args)
   {
-  System.out.println("Fibonacci series");
+  
         int a = 0;
         int b = 1;
         int n = 10;
         int sum = 0;
-        
+         ArrayList<Integer> arr = new ArrayList<Integer>();
         for(int i=1;i<=n;i++){
-            sum = a + b;
+          //System.out.println(a); --prints each element of series line by line
+            arr.add(a);  
+          sum = a + b;
             a = b;
             b = sum;
-            System.out.println(a);
+           //Series
+        System.out.println("Fibonacci Series: "+arr);
+        //Retreive nth element
+        int input = 8;
+        System.out.println(input+"th Element in series is "+arr.get(input-1));
+        //sum till nth element
+        int value = 13;
+        int sumOfVal=0;
+        for (int j=0;j<=arr.indexOf(value);j++){
+            sumOfVal+=arr.get(j);
+        }
+        System.out.println("Sum of elements in series upto value "+value+" is "+sumOfVal);
+        
+        
 
   }
   }
